22a91b16aba94b3c79d5b857a26cca4da0c18a7f
PalakPrajapati346 <palak.jp.prajapati@gmail.com>
2025-02-25 23:58:06 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,28 +1,16 @@
 import torch
-import cv2
-from PIL import Image
-import matplotlib.pyplot as plt
-
+from yolov5 import YOLOv5
 # Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
+import pathlib 
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+model = torch.hub.load('yolov5', 'custom', path='code\\best.pt', source="local", force_reload=True)
+# Perform inference on an image
+results = model('close-up-dirt-ground-wit-lot-.jpg')
 
-# Image path for inference
-image_path = 'dataset\close-up-dirt-ground-with-lot-.jpg'  # Replace with the path to your image
-
-# Perform inference
-results = model(image_path)
-
-# Print detected objects
+# Print results
 results.print()
 
 # Show results
-results.show()
-
-# Save annotated image
-results.save(save_dir='output')  # Save results in 'output' folder
-
-# Display annotated image using OpenCV (optional)
-img = cv2.imread(f'output/{image_path.split("/")[-1]}')
-cv2.imshow('Detected Objects', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+results.show()  # Displays the image with annotations
+pathlib.PosixPath=temp
